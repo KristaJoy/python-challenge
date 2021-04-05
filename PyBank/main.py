034f147b2e1d41d5fa844b21063c4cd21d786c79
Krista Joy Johnson <kristajoyjohnson@Kristas-iMac.local>
2021-04-04 19:06:56 -0700

    add to code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,42 +1,45 @@
 import os
 import csv
 
+#import
 # import csv data from this path
-csvpath = os.path.join("Resources", "budget_data.csv")
+csvpath = os.path.join("python-challenge", "PyBank", "Resources", "budget_data.csv")
 
 #open csv
-with open(csvpath) as csvfile:
+with open(csvpath, 'r') as csvfile:
    #read csv
     csvreader = csv.reader(csvfile, delimiter=',')
     #skip header
     next(csvreader, None)
     
+    #define variables
     total_months = 0
+    net_total = 0
+    
  #read through the rows
     for row in csvreader:
-    #The total number of months included in the dataset
-    #count the rows
+        #Count the total number of months
         total_months += 1
+        
+        #add up all the values in the rows
+        numlist = int(row[1])
+        net_total += numlist
+       
+        #calculate average change
+        line = float(row[1])
+        change = line - (line+1)
+  
+    averagechange = change/line * 100
+        #medianlow = statistics.median_high(
+        #medianhigh = statistics.median_low(
+        
+print("Financial Analysis")
+print("-------------------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${net_total}")
+print(f"Average Change: ${averagechange}")
+#print(f"Greatest Increase in Profits: max{list name}")
+#print(f"Greatest Decrease in Profits: min{list name}")
 
-#for rows in csvreader:
-   # r = len(rows)
-   # total = 0
-   # for row in rows:
-       # total += row # this euqals total = total + row
 
-#The net total amount of "Profit/Losses" over the entire period
-#add up the column
-
-#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-# VVV the easiest way to solve average is this:
-
-#import statistics
-#def mean(numlist):
-    #print(statistics.mean(numlist))
-
-#The greatest increase in profits (date and amount) over the entire period
-# x = max(rows?)
-
-#The greatest decrease in losses (date and amount) over the entire period
-# y = min(rows?)
-
+# old number - new number / old number x 100 is the percentage change formula
