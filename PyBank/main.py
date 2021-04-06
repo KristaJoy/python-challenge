f7ec2458b5779aa8662c044c47060fb82b49b562
Krista Joy Johnson <kristajoyjohnson@Kristas-iMac.local>
2021-04-05 18:01:49 -0700

    almost finished code

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,43 +3,49 @@ import csv
 
 #import
 # import csv data from this path
-csvpath = os.path.join("python-challenge", "PyBank", "Resources", "budget_data.csv")
+csvpath = os.path.join("Resources", "budget_data.csv")
 
 #open csv
 with open(csvpath, 'r') as csvfile:
-   #read csv
+#read csv
     csvreader = csv.reader(csvfile, delimiter=',')
-    #skip header
+#skip header
     next(csvreader, None)
     
-    #define variables
+#define variables
     total_months = 0
     net_total = 0
-    
- #read through the rows
+    profitlosses = []
+    months = []
+#read through the rows
     for row in csvreader:
-        #Count the total number of months
+#Count the total number of months
         total_months += 1
         
-        #add up all the values in the rows
+#add up all the values in the rows
         numlist = int(row[1])
         net_total += numlist
-       
-        #calculate average change
-        line = float(row[1])
-        change = line - (line+1)
-  
-    averagechange = change/line * 100
-        #medianlow = statistics.median_high(
-        #medianhigh = statistics.median_low(
+        profitlosses.append(int(row[1]))
+        months.append(str(row[0]))
+        
+#calculate average change
+month_changes = []
+for i in range(0,(len(profitlosses)-1)):
+    month_changes.append(profitlosses[i+1] - profitlosses[i])
+
+#calculate average changes
+averagechange = sum(month_changes)/(total_months-1)
+
+#calculate the highest and lowest
+greatinc = max(month_changes)
+monthinc = months[month_changes.index(greatinc)+1]
+greatdec = min(month_changes)
+monthdec = months[month_changes.index(greatdec)+1]
         
 print("Financial Analysis")
 print("-------------------------------------")
 print(f"Total Months: {total_months}")
 print(f"Total: ${net_total}")
 print(f"Average Change: ${averagechange}")
-#print(f"Greatest Increase in Profits: max{list name}")
-#print(f"Greatest Decrease in Profits: min{list name}")
-
-
-# old number - new number / old number x 100 is the percentage change formula
+print(f"Greatest Increase in Profits: ({monthinc}) {greatinc}")
+print(f"Greatest Decrease in Profits: ({monthdec}) {greatdec}")
